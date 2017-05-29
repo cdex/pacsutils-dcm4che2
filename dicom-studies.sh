8165ae7302413b762a565d6583a16a79fc61b791
Kyoma Takahashi <kyoma@jamstec.go.jp>
2017-05-29 16:04:27 +0900

    Fixed the following issue. The study list is queried by   dcmqr ${SEND_AET} -L${RETV_AET} -q00100020="$patid" for each patient without patient's name. Therefore, when there are some different patients with the same patient's ids, such a duplication occurs in the study list file.

--- dicom-studies.sh
+++ dicom-studies.sh
@@ -10,13 +10,9 @@
 
 day=`date +%Y-%m-%d`
 study=study-list.$day
-patient=patient-list.$day
 if [ -f ${WORK_DIR}/$study ] ; then
   echo ${WORK_DIR}/$study
   exit 1 
-fi
-if [ -f ${WORK_DIR}/$patient ] ; then
-  rm -f ${WORK_DIR}/$patient
 fi
 #09:52:40,995 INFO   - Query Response #2465:
 #(0008,0005) CS #10 [ISO_IR 100] Specific Character Set
@@ -33,30 +29,15 @@ fi
 #(0088,0130) SH #0 [] Storage Media File-set ID
 #(0088,0140) UI #0 [] Storage Media File-set UID
 
-${CMD_BASE}/dcmqr ${SEND_AET} -L${RETV_AET} -P \
-    | sed -n '/^[0-2][0-9]:[0-5][0-9]:[0-5][0-9],[0-9]* INFO   - Query Response #\([1-9][0-9]*\):/h; /^(0010,00[12]0)/{s:^:\t:;H}; /^$/{x;/^[0-2][0-9]:[0-5][0-9]:[0-5][0-9],[0-9]* INFO   - Query Response #[1-9][0-9]*:/{s:\n::g; p}}' \
-    | sed 's/^[0-2][0-9]:[0-5][0-9]:[0-5][0-9],[0-9]* INFO   - Query Response #\([1-9][0-9]*\):\t(0010,0010) PN #[1-9][0-9]* \[\(.*\)\] Patient.s Name\t(0010,0020) LO #[1-9][0-9]* \[\(.*\)\] Patient ID$/\2\t\3/;' \
-    | sort | uniq >  ${WORK_DIR}/$patient
+${CMD_BASE}/dcmqr ${SEND_AET} -L${RETV_AET} -r PatientName -r PatientID \
+    | sed -n '/^[0-2][0-9]:[0-5][0-9]:[0-5][0-9],[0-9]* INFO   - Query Response #\([1-9][0-9]*\):/h; /^(00[012][08],[01][02][0123][80D])/{s:^:\t:;H}; /^$/{x;/^[0-2][0-9]:[0-5][0-9]:[0-5][0-9],[0-9]* INFO   - Query Response #[1-9][0-9]*:/{s:\n::g; p}}' \
+    | sed 's/^[0-2][0-9]:[0-5][0-9]:[0-5][0-9],[0-9]* INFO   - Query Response #\([1-9][0-9]*\):\t(0008,0020) DA #[1-9][0-9]* \[\(.*\)\] Study Date\t(0008,0030) TM #[1-9][0-9]* \[\(.*\)\] Study Time\t(0010,0010) PN #[1-9][0-9]* \[\(.*\)\] Patient.s Name\t(0010,0020) LO #[1-9][0-9]* \[\(.*\)\] Patient ID\t(0020,000D) UI #[1-9][0-9]* \[\(.*\)\] Study Instance UID\t(0020,0010) SH #[1-9][0-9]* \[\(.*\)\] Study ID\t(0020,1208) IS #[1-9][0-9]* \[\(.*\)\] Number of Study Related Instances$/\6\t\8\t\4.\5.\2T\3/;' \
+    > ${WORK_DIR}/$study
 if [ $? != 0 ]; then
-   echo "[Fatal Error] Can not create : patient-list" >&2
-   exit 2
+    echo "[Fatal Error] Can not create : study-list" >&2
+    exit 2 
 fi
 
-l=`cat ${WORK_DIR}/$patient | wc -l`
-for i in `seq 1 $l`
-  do
-  patientline=`head -"$i" ${WORK_DIR}/$patient | tail -1`
-  patname=`echo "$patientline" | cut -d"	" -f1`
-  patid=`echo "$patientline" | cut -d"	" -f2`
-
-  ${CMD_BASE}/dcmqr ${SEND_AET} -L${RETV_AET} -q00100020="$patid" \
-      | sed -n '/^[0-2][0-9]:[0-5][0-9]:[0-5][0-9],[0-9]* INFO   - Query Response #\([1-9][0-9]*\):/h; /^(00[02][08],[01][02][023][80D])/{s:^:\t:;H}; /^$/{x;/^[0-2][0-9]:[0-5][0-9]:[0-5][0-9],[0-9]* INFO   - Query Response #[1-9][0-9]*:/{s:\n::g; p}}' \
-      | sed 's/^[0-2][0-9]:[0-5][0-9]:[0-5][0-9],[0-9]* INFO   - Query Response #\([1-9][0-9]*\):\t(0008,0020) DA #[1-9][0-9]* \[\(.*\)\] Study Date\t(0008,0030) TM #[1-9][0-9]* \[\(.*\)\] Study Time\t(0020,000D) UI #[1-9][0-9]* \[\(.*\)\] Study Instance UID\t(0020,1208) IS #[1-9][0-9]* \[\(.*\)\] Number of Study Related Instances$/\4\t\5\t'"$patname"'.'"$patid"'.\2T\3/;' \
-      >> ${WORK_DIR}/$study
-if [ $? != 0 ]; then
-   echo "[Fatal Error] Can not create : study-list" >&2
-   exit 2 
-fi
-done
 echo ${WORK_DIR}/$study
+
 exit 0
